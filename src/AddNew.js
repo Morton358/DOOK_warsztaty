070a4fa679a2c55778105d8914dae493dc6ab3f9
Morton358 <vovankovalchuk358@gmail.com>
2018-04-02 15:19:00 +0300

    rewrite css file with using scss

--- src/AddNew.js
+++ src/AddNew.js
@@ -1,52 +1,68 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class AddNew extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      title: '',
-      author: '',
-      tags: ''
-    }
-  }
-handleClick = (event) => {
-  this.props.onSubmit(this.state)
-}
-handleChange = (event) => {
-  this.setState({
-    [event.target.id]: event.target.value
-  })
-}
-  render(){
-    return(
-      <div className="AddNew">
-        <h2>Dodaj książkę</h2>
-        <div className="AddNewInput">
-          <div className="AddNewInputTitle">
-            <p>Tytuł:</p>
-            <input id="title" type="text"
-              placeholder="np. Jądro Ciemności" onChange={this.handleChange}
-              value={this.state.title}/>
-          </div>
-          <div className="AddNewInputAuthor">
-            <p>Author:</p>
-            <input id="author" type="text"
-              placeholder="np. J.R.R. Tolkien" onChange={this.handleChange}
-              value={this.state.author}/>
-          </div>
-          <div className="AddNewInputTags">
-            <p>Tagi:</p>
-            <input id="tags" type="text"
-              placeholder="np. fantastyka" onChange={this.handleChange}
-              value={this.state.tags}/>
-          </div>
-
-        </div>
-        <br/>
-        <button className="ButtonAdd" type="submit" onClick={this.handleClick}>dodaj</button>
-      </div>
-
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			title: "",
+			author: "",
+			tags: ""
+		};
+	}
+	handleClick = event => {
+		this.props.onSubmit(this.state);
+	};
+	handleChange = event => {
+		this.setState({
+			[event.target.id]: event.target.value
+		});
+	};
+	render() {
+		return (
+			<div className="AddNew">
+				<h2>Dodaj książkę</h2>
+				<div className="AddNewInput">
+					<div className="AddNewInputTitle">
+						<p>Tytuł:</p>
+						<input
+							id="title"
+							type="text"
+							placeholder="np. Jądro Ciemności"
+							onChange={this.handleChange}
+							value={this.state.title}
+						/>
+					</div>
+					<div className="AddNewInputAuthor">
+						<p>Author:</p>
+						<input
+							id="author"
+							type="text"
+							placeholder="np. J.R.R. Tolkien"
+							onChange={this.handleChange}
+							value={this.state.author}
+						/>
+					</div>
+					<div className="AddNewInputTags">
+						<p>Tagi:</p>
+						<input
+							id="tags"
+							type="text"
+							placeholder="np. fantastyka"
+							onChange={this.handleChange}
+							value={this.state.tags}
+						/>
+					</div>
+				</div>
+				<br />
+				<button
+					className="ButtonAdd"
+					type="submit"
+					onClick={this.handleClick}
+				>
+					dodaj
+				</button>
+			</div>
+		);
+	}
 }
 export default AddNew;
--- src/Filtr.js
+++ src/Filtr.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-
-const filtr = (props) => {
-  return (
-    <div className="Filtr">
-      {props.children}
-      {props.getTags().map((elem) => {
-        return (
-          <label>
-            <input type="checkbox" //onChange={(event) =>
-              //props.handleCheck(event, elem)}
-              checked/>
-            {JSON.stringify(elem)}
-          </label>
-        )
-      })
-      }
-    </div>
-  )
-}
+const filtr = props => {
+    return (
+        <div className="Filtr">
+            {props.children}
+            {props.getTags().map((elem, index) => {
+                return (
+                    <label key={index}>
+                        <input
+                            type="checkbox" //onChange={(event) =>
+                            //props.handleCheck(event, elem)}
+                            checked
+                        />
+                        {JSON.stringify(elem)}
+                    </label>
+                );
+            })}
+        </div>
+    );
+};
 
 export default filtr;
